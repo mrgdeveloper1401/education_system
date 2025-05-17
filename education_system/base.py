8915f88dea04e58b6ac2100add76ac6879bc085c
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-05-17 17:57:57 -0400

    edit bit pay callback url

--- education_system/base.py
+++ education_system/base.py
@@ -274,4 +274,4 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 BITPAY_MERCHANT_ID=config("GATEWAY_ID", cast=str)
-BITPAY_CALLBACK_URL='https://codeima.ir/'
+BITPAY_CALLBACK_URL='https://codeima.ir/p-student/subscription'
