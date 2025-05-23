8ea5e3e4a83707318b6edd0f666cb8fb9c337e5d
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-05-23 14:14:04 -0400

    .

--- accounts/models.py
+++ accounts/models.py
@@ -253,7 +253,7 @@ class BestStudent(CreateMixin, UpdateMixin, SoftDeleteMixin):
                                                                                            " از یک مگابایت باشد"))
     is_publish = models.BooleanField(default=True)
     description = models.CharField(max_length=500, null=True)
-    attributes = ArrayField(models.CharField(max_length=20), null=True)
+    attributes = ArrayField(models.CharField(max_length=100), null=True)
 
     class Meta:
         db_table = 'best_student'
