3b1a29069d6568f0958a0de6e324b0575de50583
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-11-03 21:06:31 +0000

    edit worker gunicorn with by async

--- education_system/asgi.py
+++ education_system/asgi.py
@@ -18,4 +18,18 @@ if debug_mode:
 else:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "education_system.envs.production")
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+
+async def application(scope, receive, send):
+    if scope['type'] == 'lifespan':
+        while True:
+            message = await receive()
+            if message['type'] == 'lifespan.startup':
+                # print("✅ Django ASGI startup event received")
+                await send({'type': 'lifespan.startup.complete'})
+            elif message['type'] == 'lifespan.shutdown':
+                # print("❌ Django ASGI shutdown event received")
+                await send({'type': 'lifespan.shutdown.complete'})
+                return
+    else:
+        await django_application(scope, receive, send)
--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-gunicorn education_system.wsgi -w 4 -b 0.0.0.0:8000
+gunicorn core.asgi:application -k uvicorn_worker.UvicornWorker -w 3 -b 0.0.0.0:8000
