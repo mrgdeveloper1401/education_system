e36032a9a7e9928dfd09dad24a2695b39578165a
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-11-03 21:19:49 +0000

    edit start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-gunicorn core.asgi:application -k uvicorn_worker.UvicornWorker -w 3 -b 0.0.0.0:8000
+gunicorn education_system.asgi:application -k uvicorn_worker.UvicornWorker -w 3 -b 0.0.0.0:8000
