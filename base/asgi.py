3e23ae9450507ac5465fc5a0b95386cfd94fc422
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2026-08-29 02:16:06 +0330

    edit start.sh

--- base/asgi.py
+++ base/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI config for education_system project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.6/howto/deployment/asgi/
-"""
-
 import os
 
 
